48afe8c5ec44a278320d613892ce6b84a722e038
Riva <z.riva.hua@gmail.com>
2025-05-10 15:55:52 -0400

    added daytime/nighttime sprite, added additional capy sprites, linked pethome activity to app flow

--- app/src/main/java/com/example/finalproject/PethomeActivity.kt
+++ app/src/main/java/com/example/finalproject/PethomeActivity.kt
@@ -1,11 +1,32 @@
 package com.example.finalproject
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class PethomeActivity : AppCompatActivity() {
+    private lateinit var pethomeView : PethomeView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) { //we want to get decorView to set the height of the rect for height
+        super.onWindowFocusChanged(hasFocus)
+        buildViewByCode()
+    }
+
+    fun buildViewByCode() {
+        var width: Int = resources.displayMetrics.widthPixels
+        var height: Int = resources.displayMetrics.heightPixels
+        var rectangle: Rect = Rect(0, 0, 0, 0)
+        window.decorView.getWindowVisibleDisplayFrame(rectangle)
+        var statusBar: Int = rectangle.top
+
+        //add adview bar below, subtract from height
+
+        pethomeView = PethomeView(this, width, height - statusBar)
+        setContentView(pethomeView)
     }
 }
--- app/src/main/java/com/example/finalproject/PethomeView.kt
+++ app/src/main/java/com/example/finalproject/PethomeView.kt
@@ -1,4 +1,34 @@
 package com.example.finalproject
 
-class PethomeView {
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.view.View
+
+class PethomeView(context: Context, private val width: Int, private val height: Int) : View(context) {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val daytime: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.daytime)
+    private val nighttime: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.nighttime)
+    private var useNight = false
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        // pick which Bitmap to draw
+        val bg = if (useNight) nighttime else daytime
+
+        // draw it stretched to fill the view bounds
+        val srcRect = Rect(0, 0, bg.width, bg.height)
+        val dstRect = Rect(0, 0, width, height)
+        canvas.drawBitmap(bg, srcRect, dstRect, paint)
+
+    }
+
+    fun toggleDayNight() {
+        useNight = !useNight
+        invalidate()
+    }
 }
